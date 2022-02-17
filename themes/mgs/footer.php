3fdd7797166aa1455f2898cd1d1fbede7e128eb7
ting.ma <tingma16@gmail.com>
2022-02-17 12:35:23 -0500

    add footer content

--- themes/mgs/footer.php
+++ themes/mgs/footer.php
@@ -12,6 +12,34 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="site-footer-extra">
+			<div class="quick-link">
+				<p>Quick Link</p>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Shop</li>
+					<li>Blog</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+			<div class="subscribe">
+				<h3>Subscribe to our newsletter!</h3>
+				<input type="text">
+				<button type="button">Subscribe</button>
+
+			</div>
+			<div class="contac-link">
+					<p>Contact</p>
+					<ul>
+						<li>2000 Simcoe St N, </li>
+						<li>Oshawa, ON L1G 0C5</li>
+						<li>info@mygirl.com</li>
+						<li>(666)777-9999</li>
+					</ul>
+				</div>
+		</div>
+
 		<div class="site-info">
 			<?php
 				/* translators: 1: Copyright, 2: Site. */
