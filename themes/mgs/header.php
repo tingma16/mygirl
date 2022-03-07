00f5ef2c0a2fa5830a3fb9ffd32ec74892944301
ting.ma <tingma16@gmail.com>
2022-03-07 00:30:16 -0500

    customize social media menu for facebook

--- themes/mgs/header.php
+++ themes/mgs/header.php
@@ -65,15 +65,30 @@
 				);
 			}
 
-			if( has_nav_menu('menu-social')){
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-social',
-					)
-				);
-			}
+			// if( has_nav_menu('menu-social')){
+			// 	wp_nav_menu(
+			// 		array(
+			// 			'theme_location' => 'menu-social',
+			// 		)
+			// 	);
+			// }
 
 			
 			?>
+			<ul class="social-media-menu">
+				<?php if( ! empty (get_theme_mod ( 'mgs_facebook_url' ) ) && ! empty (get_theme_mod ( 'mgs_facebook_title' ) ) ) { ?>
+				<li>
+					<a href="<?php echo esc_url( get_theme_mod('mgs_facebook_url' ) );?>" aria-label="<?php echo ! empty (get_theme_mod( 'mgs_facebook_icon' ) ) ?  get_theme_mod('mgs_facebook_title' ) :''; ?>">
+						<?php 
+						if ( ! empty (get_theme_mod( 'mgs_facebook_icon' ) ) ) {
+							echo wp_get_attachment_image( get_theme_mod( 'mgs_facebook_icon' ), array('30','30') );
+						}else{
+						echo get_theme_mod('mgs_facebook_title' );
+						}
+						?>
+					</a>
+				</li>
+				<?php } ?>
+			</ul>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
